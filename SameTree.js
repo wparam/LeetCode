acc3c61a49435fe3374beb8baf24dafa97aa19d7
wparam@hotmail.com <wparam@hotmail.com>
2016-06-07 22:49:36 +0800

    add same tree

--- SameTree.js
+++ SameTree.js
@@ -16,6 +16,48 @@
  * @param {TreeNode} q
  * @return {boolean}
  */
+'use strict';
+const util = require('util');
+var tre = require('./lib/tree.js');
 var isSameTree = function(p, q) {
+    var serialize = function(node){
+        var result = [];
+        if(!node)
+            return [null];
+        result.push(node.val);
+        if(!node.left && !node.right)
+            return [node.val];
+        result = [].concat(result, serialize(node.left));
+        result = [].concat(result, serialize(node.right));
+        return result;
+    };
     
+    var parry = serialize(p),
+        plength = parry.length,
+        result = true,
+        qarry = serialize(q);
+    console.log(parry);
+    console.log(qarry);
+    if(parry.length!==qarry.length)
+        return false;
+    for(var i = 0; i<plength; i++){
+        if(parry[i]!==qarry[i]){
+            result = false;
+            return false;
+        }
+    }
+    return result;
 };
+
+console.log(isSameTree(tre.serializeTree([2,3,null,1,3,1,2,7,null]),
+    tre.serializeTree([2,4,null,1,3,1,2,7,null])));
+    
+console.log(isSameTree(tre.serializeTree([2,3,null,1,3,1,2,7,null]),
+    tre.serializeTree([2,3,null,1,1,3,2,7,null])));
+console.log(isSameTree(tre.serializeTree([2,3,null,1,3,1,2,7,null]),
+    tre.serializeTree([2,4,null,1,3,1,2,7,0])));
+console.log(isSameTree(tre.serializeTree([2,3,null,1,3,1,2,7,null]),
+    tre.serializeTree([2,3,null,1,3,1,2,7,null])));
+console.log(isSameTree(tre.serializeTree([]),
+    tre.serializeTree([0])));
+console.log(tre.serializeTree([]));
--- test.js
+++ test.js
@@ -19,7 +19,20 @@ var invertTree = function(root) {
     return root;
 };
 
-var tree = tre.serializeTree([2,3,null,1]);
-var result = invertTree(tree);
-console.log(tre.deserializeTree(result));
+var middletravs = function(node){
+    var result = [];
+    if(!node || !node.val)
+        return [null];
+    result.push(node.val);
+    if(!node.left && !node.right)
+        return [node.val];
+    result = [].concat(result, middletravs(node.left));
+    result = [].concat(result, middletravs(node.right));
+    return result;
+};
+
+var tree = tre.serializeTree([2,3,null,1,3,1,2,7,null]);
+console.log(middletravs(tree));
+// var result = invertTree(tree);
+// console.log(tre.deserializeTree(result));
 //console.log(JSON.stringify(tre.serializeTree([1,2,3,4,6,7,8])));
