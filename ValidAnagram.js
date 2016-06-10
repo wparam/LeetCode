bd4bc7ab3852879bea7f371ff1630c4b924bedb2
wparam@hotmail.com <wparam@hotmail.com>
2016-06-10 22:30:56 +0800

    add valid anagram

--- ValidAnagram.js
+++ ValidAnagram.js
@@ -18,4 +18,31 @@
 var isAnagram = function(s, t) {
     if(s.length !== t.length)
         return false;
+    var sum = 0,
+        ss = 0,
+        tt = 0,
+        sSum = 0, 
+        tSum = 0,
+        s3 = 0,
+        t3 = 0;
+    for(var i = 0, l = s.length; i<l; i++){
+        var num = s.charCodeAt(i) - t.charCodeAt(i);
+        sum += num;
+        ss += s.charCodeAt(i);
+        tt += t.charCodeAt(i);
+        sSum += Math.pow(s.charCodeAt(i), 2);
+        tSum += Math.pow(t.charCodeAt(i), 2);
+        s3 += Math.pow(s.charCodeAt(i), 3);
+        t3 += Math.pow(t.charCodeAt(i), 3);
+    }
+    if(sum !== 0)
+        return false;
+    return (sSum === tSum) && (ss === tt) && (s3 === t3);
 };
+
+// "vbnxkji"
+// "wqdtegp"
+
+console.log(isAnagram('vbnxkji', 'wqdtegp'));
+
+// console.log(isAnagram('hqbqo', 'lsnma'));
