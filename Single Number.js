aca31580cd3a6af6b94ef78a7610a175561dd0f6
wparam@hotmail.com <wparam@hotmail.com>
2016-09-10 16:14:39 +0800

    add singlenum

--- Single Number.js
+++ Single Number.js
@@ -7,7 +7,37 @@
  * @param {number[]} nums
  * @return {number}
  */
+var assert = require('assert');
 var singleNumber = function(nums) {
+    var arr = [];
     for(var i=0, l=nums.length; i<l; i++)
-        if(nums[i])
+        if(arr[nums[i]] === undefined){
+            arr[nums[i]] = true;
+        }else {
+            arr[nums[i]] = false;
+        }
+    for(var j=0, l1=arr.length; j<l1; j++)
+        if(arr[j] === true)
+            return j;
+    return ;
 };
+
+var singleNumberObject = function(nums) {
+    var arr = {};
+    for(var i=0, l=nums.length; i<l; i++)
+        if(arr[nums[i]] === undefined){
+            arr[nums[i]] = true;
+        }else {
+            arr[nums[i]] = false;
+        }
+    for(var j in arr){
+        if(arr.hasOwnProperty(j) && arr[j] === true)
+            return parseInt(j);
+    }
+    return ;
+};
+
+console.log(singleNumber([9,1,3,2,1,5,9,2,5,3,8, 8 , -1]));
+
+console.log(singleNumberObject([9,1,3,2,1,5,9,2,5,3,-1]));
+
