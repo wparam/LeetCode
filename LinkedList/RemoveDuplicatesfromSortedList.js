d87c864f513a304a40efaf89b8327901e6fd4d07
wparam@hotmail.com <wparam@hotmail.com>
2016-09-12 22:44:24 +0800

    up

--- LinkedList/RemoveDuplicatesfromSortedList.js
+++ LinkedList/RemoveDuplicatesfromSortedList.js
@@ -15,6 +15,28 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+var util = require('util');
 var deleteDuplicates = function(head) {
-    
+    var headanchor = head;
+    if(!head || !head.next)
+        return head;
+    while(head.next){
+        if(head.next.val === head.val){
+            if(!head.next.next)
+                head.next = null;
+            else
+                head.next = head.next.next;
+        }
+        else
+            head = head.next;
+    }
+    return headanchor;
 };
+
+
+var h2 = {val:2, next:null};
+var h11 = {val:1, next:h2};
+var h1 = {val:1, next:h11};
+var h01 = {val:0, next:h1};
+var h0 = {val: 0, next: h01};
+console.dir(util.inspect(deleteDuplicates(h0), false, null));
