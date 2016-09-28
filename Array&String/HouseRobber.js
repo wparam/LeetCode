ac5716297ee78d759a7b8c521022d1ab5e7155ee
wparam@hotmail.com <wparam@hotmail.com>
2016-09-28 23:17:01 +0800

    up

--- Array&String/HouseRobber.js
+++ Array&String/HouseRobber.js
@@ -14,18 +14,21 @@
  * the N time's monty f(n) == max(f(n-1), f(n-2) + n)
  */
 var rob = function(nums) {
+    var _g_max = {};
     if(!nums || nums.length === 0)
         return 0;
     if(nums.length === 1)
         return nums[0];
     if(nums.length ===2)
         return Math.max(nums[0], nums[1]);
-    //return Math.max(rob(nums.slice(0, nums.length-1)), rob(nums.slice(0, nums.length-2)) + nums[nums.length-1]);
-    var i = nums.length-1;
-    while(i>0){
+    var max = 0 ;
+    for(var i=0, l=nums.length; i<l;i++){
         
-        i--;
     }
+    return Math.max(l1, l2 + nums[nums.length-1]);
+    //return Math.max(rob(nums.slice(0, nums.length-1)), rob(nums.slice(0, nums.length-2)) + nums[nums.length-1]);
+    
+    
 };
 
 console.log(rob([183,219,57,193,94,233,202,154,65,240,97,234,100,249,186,66,90,238,168,128,177,235,50,81,185,165,217,207,88,80,112,78,135,62,228,247,211]));
