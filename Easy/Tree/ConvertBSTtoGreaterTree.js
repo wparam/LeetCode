c8521465f1621d8db1ca8f69ab0d6e01e1f4edab
wparam <wparam@hotmail.com>
2017-07-31 18:46:31 -0400

    not finish

---
+++ Easy/Tree/ConvertBSTtoGreaterTree.js
@@ -0,0 +1,45 @@
+// Given a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus sum of all keys 
+//greater than the original key in BST.
+
+// Example:
+
+// Input: The root of a Binary Search Tree like this:
+//               5
+//             /   \
+//            2     13
+
+// Output: The root of a Greater Tree like this:
+//              18
+//             /   \
+//           20     13
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+
+'use strict';
+const util = require('util');
+var tre = require('../../lib/tree.js');
+var convertBST = function(root) {
+    if(!root)
+        return 0;
+    if(root.left){
+        root.val += root.left.val;    
+        root.left.val += root.val + (root.right ? root.right.val: 0);
+    }
+    convertBST(root.left);        
+    convertBST(root.right);
+};
+
+var node = tre.serializeTree([5, 2, 13]);
+convertBST(node);
+console.log(tre.deserializeTree(node));
