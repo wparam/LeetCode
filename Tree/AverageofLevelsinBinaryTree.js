889bb1034136ea1e4b5b20323796fda858af31e6
wparam <wparam@hotmail.com>
2017-07-31 16:49:52 -0400

    finish AverageofLevelsinBinaryTree

--- Tree/AverageofLevelsinBinaryTree.js
+++ Tree/AverageofLevelsinBinaryTree.js
@@ -29,17 +29,33 @@
 'use strict';
 const util = require('util');
 var tre = require('../lib/tree.js');
+
 var averageOfLevels = function(root) {
     if(!root)
         return 0;
-    let result = [];
-    let leftval = root.left && root.left.val ? root.left && root.left.val : 0;
-    let rightval = root.right && root.right.val ? root.right && root.right.val : 0;
-    if(leftval!==0 || rightval !== 0)
-        result.push((leftval + rightval)/2);
-    result = [].concat(result, averageOfLevels(root.left), averageOfLevels(root.right));
-    console.log(result);
-    return result;
+    let sum = [];
+    let count = [];
+    var foo = function(node, dept){
+        if(!node || node.val === null)
+            return;
+        if(sum[dept]=== undefined || count[dept]===undefined){
+            sum[dept] = 0;
+            count[dept] = 0;
+        }
+        sum[dept] += node.val;
+        count[dept] += 1;
+        dept++;
+        if(node.left)
+            foo(node.left, dept);
+        if(node.right)
+            foo(node.right, dept);
+    }
+    foo(root, 0);
+    for(let i = 0, j = sum.length; i<j; i++){
+        sum[i] = sum[i]/count[i];
+    }
+    return sum;
 };
 
-console.log(tre.deserializeTree(averageOfLevels(tre.serializeTree([2,3,null,1,3,1,2,7,null]))));
+// console.log(tre.deserializeTree(averageOfLevels(tre.serializeTree([2,3,null,1,3,1,2,7,null]))));
+console.log(averageOfLevels(tre.serializeTree([2,3,null,1,3,1,2,7,null])));
