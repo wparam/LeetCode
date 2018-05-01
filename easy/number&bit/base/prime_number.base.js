a21b3d85c8f49fcd4281b1af40458ee3edd771b4
wparam <wparam@hotmail.com>
2018-05-01 16:49:05 -0400

    up

--- easy/number&bit/base/prime_number.base.js
+++ easy/number&bit/base/prime_number.base.js
@@ -53,28 +53,57 @@ var prime2 = function(n){
  * @retuen []
  */
 var prime3 = function(n){
-    for(let i = 2; i<n; i++){
-        console.log(i);
+    let result = [false, false];
+    for(let i = 2; i<=n; i++){
+        result[i] = true;
     }
-
+    for(let i = 2; i<n; i++){
+        for(let j = i; i * j <=n; j++){
+            result[i*j] = false;
+        }
+    }
+    console.log(result.filter(n => n).length);
 
 };
 
-var s = new Date();
+/**
+ * Find all the prime numbers < n
+ * Update Shai method
+ * @param  {} n
+ * @retuen []
+ */
+var prime4 = function(n){
+    let result = [false, false];
+    let sqt = Math.sqrt(n);
+    for(let i = 2; i<=n; i++){
+        result[i] = true;
+    }
+    for(let i = 2; i<=sqt; i++){
+        for(let j = i; i * j <=n; j++){
+            result[i*j] = false;
+        }
+    }
+    console.log(result.filter(n => n).length);
 
-prime1(1000000);
-console.log('use %s s', (new Date() - s)/1000);
+};
 
-var s1 = new Date();
+// var s = new Date();
 
-prime2(1000000);
-console.log('use %s s', (new Date() - s1)/1000);
+// prime1(10000000);
+// console.log('use %s s', (new Date() - s)/1000);
 
-// console.log(new Date());
-// console.log(prime3());
-// console.log(new Date());
+// var s1 = new Date();
 
+// prime2(10000000);
+// console.log('use %s s', (new Date() - s1)/1000);
 
+var s2 = new Date();
+prime3(10000000);
+console.log('use %s s', (new Date() - s2)/1000);
+
+var s3 = new Date();
+prime4(10000000);
+console.log('use %s s', (new Date() - s3)/1000);
 
 
 
