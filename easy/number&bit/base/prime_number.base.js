4a19f640dd8d11b1883f1c1b2ebad43e5269f8af
wparam <wparam@hotmail.com>
2018-05-01 14:07:30 -0400

    up

--- easy/number&bit/base/prime_number.base.js
+++ easy/number&bit/base/prime_number.base.js
@@ -5,11 +5,20 @@
  * @retuen []
  */
 var prime1 = function(n){
-    for(let i = 2; i<n; i++){
-        console.log(i);
+    let result = [2],
+        flag = true;
+    for(let i = 3; i<n; i+=2){
+        flag = true;
+        for(let j = 3; j<=Math.sqrt(i); j++){
+            if(i % j === 0){
+                flag = false;
+                break;
+            }
+        }
+        if(flag)
+            result.push(i);
     }
-
-
+    console.log(result.length);
 };
 
 
@@ -20,11 +29,21 @@ var prime1 = function(n){
  * @retuen []
  */
 var prime2 = function(n){
-    for(let i = 2; i<n; i++){
-        console.log(i);
+    let result = [2];
+    for(let i = 3; i<n; i+=2){
+        if(isPrime(i))
+            result.push(i);
     }
 
-
+    function isPrime(m){
+        for(let j = 3; j<=Math.sqrt(m); j++){
+            if(m % j === 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    console.log(result.length);
 };
 
 /**
@@ -41,15 +60,19 @@ var prime3 = function(n){
 
 };
 
-console.log(new Date());
-console.log(prime1());
-console.log(new Date());
+var s = new Date();
 
-console.log(prime2());
+prime1(1000000);
+console.log('use %s s', (new Date() - s)/1000);
 
-console.log(new Date());
-console.log(prime3());
-console.log(new Date());
+var s1 = new Date();
+
+prime2(1000000);
+console.log('use %s s', (new Date() - s1)/1000);
+
+// console.log(new Date());
+// console.log(prime3());
+// console.log(new Date());
 
 
 
