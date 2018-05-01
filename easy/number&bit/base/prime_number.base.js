e4ff2bd2a921674d6ec48cf33d20f5d7b83a42bb
wparam <wparam@hotmail.com>
2018-05-01 12:18:54 -0400

    add base prime number solution

---
+++ easy/number&bit/base/prime_number.base.js
@@ -0,0 +1,59 @@
+/**
+ * Find all the prime numbers < n
+ * Normal solution
+ * @param  {} n
+ * @retuen []
+ */
+var prime1 = function(n){
+    for(let i = 2; i<n; i++){
+        console.log(i);
+    }
+
+
+};
+
+
+/**
+ * Find all the prime numbers < n
+ * Normal solution, using function
+ * @param  {} n
+ * @retuen []
+ */
+var prime2 = function(n){
+    for(let i = 2; i<n; i++){
+        console.log(i);
+    }
+
+
+};
+
+/**
+ * Find all the prime numbers < n
+ * Shai method
+ * @param  {} n
+ * @retuen []
+ */
+var prime3 = function(n){
+    for(let i = 2; i<n; i++){
+        console.log(i);
+    }
+
+
+};
+
+console.log(new Date());
+console.log(prime1());
+console.log(new Date());
+
+console.log(prime2());
+
+console.log(new Date());
+console.log(prime3());
+console.log(new Date());
+
+
+
+
+
+
+
