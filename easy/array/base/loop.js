f2ef1849905f33c5e75f6c8e4eb5dd76843b31c8
wparam <wparam@hotmail.com>
2018-05-14 22:23:41 -0400

    add

---
+++ easy/array/base/loop.js
@@ -0,0 +1,15 @@
+var maoPao = function(arr, asc){
+    let temp = 0;
+    for(let i = 0, l = arr.length; i<l; i++){
+        for(let j = i; j<l; j++){
+            if(arr[i] > arr[j]){
+                temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+    return arr;
+};
+
+console.log(maoPao([4,2,1,3,7,4,8,4,6,9]));
