ca02d926fd10445e525eef4e451022f4212540de
wparam <wparam@hotmail.com>
2018-05-24 17:26:33 -0400

    up

--- easy/array/base/stack.js
+++ easy/array/base/stack.js
@@ -2,31 +2,43 @@ class Stack{
     constructor(){
         this.arr = [];
     }
+    readPop() {
+        if(this.arr.length === 0)
+            return null;
+        return this.arr[this.arr.length - 1];
+    }
     pop(){
         this.arr.pop();
     }
     push(ele){
         this.arr.push(ele);
     }
+    clear() {
+        this.arr = [];
+    }
 }
-const signs = [
-    
-]
+
+const signs = [ ')', '*', '/', '+', '-', '(' ];
+
 var transform = (form) => {
+    let stack = new Stack();
     //transform mid to post
     let result = [];
     for(let i = 0, l=form.length; i<l; i++){
-        let j = i;
-        while(j<l && /\d/gi.test(form[j])){
-            j++;
+        if(/\d/gi.test(form[i])){
+            let j = i;
+            while(j<l && /\d/gi.test(form[j])){
+                j++;
+            }
+            result.push(parseInt(form.substring(i, j)));
+            i = --j;
+        }else{
+            console.log(form[i]);
         }
-        result.push(parseInt(form.substring(i, j)));
-        i = j;
     }
     return result;
 };
 
-var readNumber
 
 var calculate = (form) => {
 
@@ -37,4 +49,4 @@ var calculator = (form) => {
 };
 
 // console.log(calculator('9+(2-1)*3-6/2'));
-console.log(transform('9+(20-1)*3-6/2'));
+console.log(transform('9+(20-1)*3-6/2')); //should output 9 20 1 - 3 * + 6 2 / -
