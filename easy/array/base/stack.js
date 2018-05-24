936af93ace4cac5f9d5ef96f6cb48de8b5b5cf07
Jason <wparam@hotmail.com>
2018-05-23 22:59:41 -0400

    up

---
+++ easy/array/base/stack.js
@@ -0,0 +1,36 @@
+class Stack{
+    constructor(){
+        this.arr = [];
+    }
+    pop(){
+        this.arr.pop();
+    }
+    push(ele){
+        this.arr.push(ele);
+    }
+}
+const signs = [
+    
+]
+var transform = (form) => {
+    //transform mid to post
+    let result = [];
+    for(let i in form){
+        if()
+    }
+
+
+    return resul
+};
+
+var readNumber
+
+var calculate = (form) => {
+
+};
+
+var calculator = (form) => {
+    return calculate(transform(form));
+};
+
+console.log(calculator('9+(2-1)*3-6/2'));
