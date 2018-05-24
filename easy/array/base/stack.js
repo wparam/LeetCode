aa270c80385a6448d6959631c3d0c3f92960e9a7
Jason <wparam@hotmail.com>
2018-05-24 13:27:13 -0400

    add stack

--- easy/array/base/stack.js
+++ easy/array/base/stack.js
@@ -15,12 +15,15 @@ const signs = [
 var transform = (form) => {
     //transform mid to post
     let result = [];
-    for(let i in form){
-        if()
+    for(let i = 0, l=form.length; i<l; i++){
+        let j = i;
+        while(j<l && /\d/gi.test(form[j])){
+            j++;
+        }
+        result.push(parseInt(form.substring(i, j)));
+        i = j;
     }
-
-
-    return resul
+    return result;
 };
 
 var readNumber
@@ -33,4 +36,5 @@ var calculator = (form) => {
     return calculate(transform(form));
 };
 
-console.log(calculator('9+(2-1)*3-6/2'));
+// console.log(calculator('9+(2-1)*3-6/2'));
+console.log(transform('9+(20-1)*3-6/2'));
