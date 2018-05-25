75cd9fd84f0b4376d7e08e42b1edc1a459fca2b1
wparam <wparam@hotmail.com>
2018-05-25 16:31:40 -0400

    finish calculator

--- easy/array/base/stack.js
+++ easy/array/base/stack.js
@@ -8,7 +8,9 @@ class Stack{
         return this.arr[this.arr.length - 1];
     }
     pop(){
-        this.arr.pop();
+        if(this.arr.length === 0)
+            return;
+        return this.arr.pop();
     }
     push(ele){
         this.arr.push(ele);
@@ -19,12 +21,27 @@ class Stack{
 }
 
 const signs = {
+    '(': 100,
     '*': 10,
     '/': 10,
-    '+': 1,
-    '-': 1
+    '+': 5,
+    '-': 5
 };
 
+
+/**
+ * pse func
+ * for loop of 'expression'
+ * {    if number
+ *         output
+ *      else
+ *         if ')' -> output until '('
+ *         if form[i] < stack.readPop() -> pop until form[i] > stack.readPop()
+ *         if form[i] > stack.readPop() -> stack.push()
+ * }
+ * if expression finish -> ouput stack
+ * @param  {} form
+ */
 var transform = (form) => {
     let stack = new Stack();
     //transform mid to post
@@ -38,24 +55,66 @@ var transform = (form) => {
             result.push(parseInt(form.substring(i, j)));
             i = --j;
         }else{
-            if(stack.readPop() < signs[form[i]] || form[i] === '(')
+            result = [].concat(result, popStackSigns(stack, form[i]));
+            if(form[i] !== ')')
                 stack.push(form[i]);
-            else {
-                
-            }
         }
     }
+    while(stack.readPop()!==null)
+        result.push(stack.pop());
     return result;
 };
 
+var popStackSigns = (stack, ch) => {
+    let result = [];
+    //special for '()'
+    if(stack.readPop() === '(')
+        return [];
+    if(ch === ')'){
+        while(stack.readPop()!==null){
+            if(stack.readPop()!=='(')
+                result.push(stack.pop());
+            else    
+                break;
+        }
+        stack.pop();
+        return result;
+    }
+    if(signs[ch] <= signs[stack.readPop()]){
+        while( signs[ch] <= signs[stack.readPop()] && stack.readPop() !== '('){
+            result.push(stack.pop());
+        }
+    }
+
+    return result;
+}
+
 
 var calculate = (form) => {
+    let stack = new Stack();
+    for(let i = 0, l = form.length; i<l; i++){
+        if(/\d/gi.test(form[i])){
+            stack.push(form[i]);
+        }
+        else{
+            let l1 = stack.pop();
+            let l2 = stack.pop();
+            stack.push(parseExpression(l2 + form[i] + l1));
+        }
+    }
+    return stack.pop(parseExpression);
+};  
 
-};
+var parseExpression = function(str){
+    return new Function('"use strict"; return (' + str + ')')();
+}
+
 
 var calculator = (form) => {
     return calculate(transform(form));
 };
 
 // console.log(calculator('9+(2-1)*3-6/2'));
-console.log(transform('9+(20-1)*3-6/2')); //should output 9 20 1 - 3 * + 6 2 / -
+console.log(calculator('9+(20-1)*3-6/2')); //should output 9 20 1 - 3 * + 6 2 / -  => 63
+console.log(calculator('2*4+(6-2)/4+8-6')); //should output 24*62-4/+8+6-     =>  11
+console.log(calculator('2*4+((6-2)/4+8)-6')); //should output 24*62-4/8++6-      =>  11
