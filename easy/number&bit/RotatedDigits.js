f562e7f6ed70feff9c599fbffcac5051f723d32a
Jason <wparam@hotmail.com>
2018-05-26 18:05:08 -0400

    add new ones

--- easy/number&bit/RotatedDigits.js
+++ easy/number&bit/RotatedDigits.js
@@ -1,6 +1,9 @@
-// X is a good number if after rotating each digit individually by 180 degrees, we get a valid number that is different from X.  Each digit must be rotated - we cannot choose to leave it alone.
+// X is a good number if after rotating each digit individually by 180 degrees, 
+//we get a valid number that is different from X.  Each digit must be rotated - we cannot choose to leave it alone.
 
-// A number is valid if each digit remains a digit after rotation. 0, 1, and 8 rotate to themselves; 2 and 5 rotate to each other; 6 and 9 rotate to each other, and the rest of the numbers do not rotate to any other number and become invalid.
+// A number is valid if each digit remains a digit after rotation. 0, 1, and 8 rotate to themselves; 
+//2 and 5 rotate to each other; 6 and 9 rotate to each other, and the rest of the numbers do not rotate to any other number 
+//and become invalid.
 
 // Now given a positive number N, how many numbers X from 1 to N are good?
 
@@ -19,7 +22,8 @@
  * @return {number}
  */
 var rotatedDigits = function(N) {
-    let sum = 0;
+    let sum = 0,
+        indx = 0;
     while(N % 10 !== 0 ){
         N = parseInt(N / 10);
     }
