711714f963bc62b8a2b38db945af5f695282e1c5
wparam <wparam@hotmail.com>
2018-05-31 18:18:50 -0400

    up

--- easy/array/Two Sum II - Input array is sorted.js
+++ easy/array/Two Sum II - Input array is sorted.js
@@ -1,6 +1,8 @@
-// Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+// Given an array of integers that is already sorted in ascending order, 
+// find two numbers such that they add up to a specific target number.
 
-// The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
+// The function twoSum should return indices of the two numbers such that they add up to the target, 
+// where index1 must be less than index2.
 
 // Note:
 
@@ -20,3 +22,5 @@
 var twoSum = function(numbers, target) {
     
 };
+
+console.log(twoSum([-1, 0], -1));
