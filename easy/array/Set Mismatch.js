db20353e82b6bb2882ba9fe2452124c819a84cd4
wparam <wparam@hotmail.com>
2018-06-21 18:15:12 -0400

    up

--- easy/array/Set Mismatch.js
+++ easy/array/Set Mismatch.js
@@ -1,3 +1,4 @@
+// [$tag=map$]
 // The set S originally contains numbers from 1 to n. But unfortunately, due to the data error, one of the numbers in the set got duplicated to another number in the set, which results in repetition of one number and loss of another number.
 
 // Given an array nums representing the data status of this set after the error. Your task is to firstly find the number occurs twice and then find the number that is missing. Return them in the form of an array.
@@ -13,23 +14,22 @@
  * @return {number[]}
  */
 var findErrorNums = function(nums) {
-    let map = new Map();
     let arr = [];
     let duplicate=-1, miss=-1;
     for(let i = 0, l=nums.length; i<l; i++ ){
-        arr[nums[i]] = 1;
-        if(map.has(nums[i])){
-            duplicate = nums[i];
-        }
-        else{
-            map.set(nums[i], 1);
-        }
+        if(arr[nums[i]]!==1)
+            arr[nums[i]] = 1;
+        else
+            arr[nums[i]]++;
     }
     for(let i = 1, l = arr.length; i<=l; i++){
-        if(arr[i]!==1){
-            miss=i;
-            break;
+        if(arr[i]>1){
+            duplicate = i;
+        }else if(arr[i]!==1 && miss===-1){
+            miss = i;
         }
     }
     return [duplicate, miss];
 };
+
+console.log(findErrorNums([1,2,2,4]));
